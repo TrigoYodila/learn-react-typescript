f5c4832d594a38c1a3659b1f480d6d4395b1536f
Trigo YODILA <trigoyodila1996@gmail.com>
2023-10-17 10:25:28 +0100

    types avancés de props

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,15 +1,17 @@
 type GreetProps = {
     name:string
-    messageCount:number
+    messageCount?:number //accessoires facultatifs
     isLoggedIn:boolean
 }
 
 export const Greet = (props:GreetProps) => {
+    //props par défaut
+    const { messageCount = 0 } = props;
     return (
       <div>
         <h2>
             {
-                props.isLoggedIn ? `Bienvenu ${props.name}! Tu as ${props.messageCount} messages non lues` : 'Bienvenu'
+                props.isLoggedIn ? `Bienvenu ${props.name}! Tu as ${messageCount} messages non lues` : 'Bienvenu'
             }
         </h2>
       </div>
--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,5 +1,5 @@
 type StatusProps = {
-    status:'loading' | 'success' | 'error'
+    status:'loading' | 'success' | 'error' //string
 }
 
 export const Status = (props:StatusProps) => {
